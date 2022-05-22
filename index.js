ea02c428ecf6065167e6b68accdab4cf7106ee63
Kaniz Fatema <kaniz.fatema@bitopibd.com>
2022-05-23 00:40:57 +0600

    connected mongodb

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e4yaz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("db-tools").collection("tools");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/', (req,res)=>{
+    res.send("Hello from server")
+})
+
+app.listen(port, ()=>{
+    console.log(`Port ${port}`)
+})
